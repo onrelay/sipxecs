f21e316cdb2d368bc4a1f02149d32a4d10954c13
iplahte <ivar.plahte@onrelay.com>
2024-06-14 15:21:00 +0200

    Stun port added to DB once too many

--- sipXconfig/etc/sipxpbx/database/nat-location.sql
+++ sipXconfig/etc/sipxpbx/database/nat-location.sql
@@ -1,7 +1,6 @@
 alter table location
   add column use_stun boolean,
   add column stun_address varchar(255),
-  add column stun_port integer,
   add column stun_interval integer,
   add column public_address varchar(255),
   add column public_port integer,
@@ -13,14 +12,12 @@ update location
     use_stun=true,
     stun_interval = 60,
     stun_address = 'freestun.net',
-    stun_port = 3478,
     public_port=5060,
     start_rtp_port=30000,
     stop_rtp_port=31000;
 
 alter table location
   alter column use_stun set not null,
-  alter column stun_port set not null,
   alter column stun_interval set not null,
   alter column public_port set not null,
   alter column start_rtp_port set not null,
@@ -28,7 +25,6 @@ alter table location
 
 alter table location
   alter column use_stun set default true,
-  alter column stun_port set default 3478,
   alter column stun_interval set default 60,
   alter column public_port set default 5060,
   alter column start_rtp_port set default 30000,
