20cf605b2df40fb5f71dff80271fd175a3da1f06
iplahte <ivar.plahte@onrelay.com>
2024-06-17 16:54:26 +0200

    Fixed stun_port regresssion

--- sipXconfig/etc/sipxpbx/database/location-add-stun-port.sql
+++ sipXconfig/etc/sipxpbx/database/location-add-stun-port.sql
@@ -1,15 +1,8 @@
-do $$ 
-    begin
-        begin
-            alter table location add column stun_port integer;
-            update location set stun_port=3478;
-            alter table location alter column stun_port set not null;
-            alter table location alter column stun_port set default 3478;
-        exception
-            when duplicate_column then raise notice 'column stun_port already exists in table location.';
-        end;
-    end;
-$$
+alter table location add column stun_port integer;
+update location set stun_port=3478;
+alter table location alter column stun_port set not null;
+alter table location alter column stun_port set default 3478;
+
 
 
 
