8ce1efcad10e2715414833b186a2bad7f2708ed5
root <root@t02.onrelay.net>
2024-06-17 21:41:45 +0000

    New stun_port attempt

--- sipXconfig/etc/sipxpbx/database/location-add-stun-port.sql
+++ sipXconfig/etc/sipxpbx/database/location-add-stun-port.sql
@@ -1,8 +1,5 @@
 alter table location drop column if exists stun_port;
-alter table location add column stun_port integer;
-update location set stun_port=3478;
-alter table location alter column stun_port set not null;
-alter table location alter column stun_port set default 3478;
+alter table location add column stun_port integer default 3478;
 
 
 
