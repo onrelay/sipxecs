7bd245c52a7e8089f62b772b1387df4d2bbb34cb
iplahte <ivar.plahte@onrelay.com>
2024-06-24 23:01:38 +0200

    New stun_port attempt

--- sipXconfig/etc/sipxpbx/database/schema.sql
+++ sipXconfig/etc/sipxpbx/database/schema.sql
@@ -796,6 +796,7 @@ create table location (
   last_attempt timestamp,
   call_traffic boolean not null default true,
   replicate_config boolean not null default true,
+  stun_port integer not null default 3478,
   primary key (location_id)
 );
 
