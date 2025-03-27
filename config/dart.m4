6f54973aa09d8cd22976baba1a5ae711002bf037
iplahte <ivar.plahte@onrelay.com>
2025-03-27 09:36:09 +0000

    Generic configure and build fixes for Rocky Linux 9

--- config/dart.m4
+++ config/dart.m4
@@ -1,29 +1,35 @@
 AC_DEFUN([CHECK_DART_SDK],
 [
-   AC_ARG_VAR(DART_HOME, [Dartlang SDK home directory])
-   AC_MSG_CHECKING([dart-sdk])
-   if test x_"${DART_HOME}" != x_
-   then
-    AC_CHECK_FILE([${DART_HOME}/bin/dart2js],,[
-     SF_MISSING_DEP([Cannot find dart2js in dart sdk at ${DART_HOME}/bin/dart2js])
+  AC_ARG_VAR(DART_HOME, [Dartlang SDK home directory])
+  AC_MSG_CHECKING([dart-sdk])
+
+  if test "x${DART_HOME}" != "x"; then
+    AC_CHECK_FILE([${DART_HOME}/bin/dart],,[
+      SF_MISSING_DEP([Cannot find dart in dart sdk at ${DART_HOME}/bin/dart])
     ])
-   else
-     candidates="${DART_HOME} /opt/dart/dart-sdk /opt/dart-sdk ~/dart-sdk"
-     for candidate in $candidates; do
-       AC_CHECK_FILE([$candidate/bin/dart2js],
-       [
-          DART_HOME=$candidate
-	     break
-      ],)
-     done
-   fi
+  else
+    candidates="/opt/dart/dart-sdk /opt/dart-sdk ~/dart-sdk"
+    for candidate in $candidates; do
+      if test -x "$candidate/bin/dart"; then
+        DART_HOME="$candidate"
+        break
+      fi
+    done
+  fi
 
-   if test x$DART_HOME == x; then
-     SF_MISSING_DEP([Cannot find dart-sdk])
-   else
-     AC_MSG_RESULT(${DART_HOME})
-   fi
+  if test "x${DART_HOME}" = "x"; then
+    SF_MISSING_DEP([Cannot find dart-sdk])
+  else
+    AC_MSG_RESULT(${DART_HOME})
+  fi
 
-   AC_SUBST(DART_HOME)
-   AC_SUBST(DART_PKGS, [${datadir}/dart])
+  SRC_DIR=`cd $srcdir && pwd`
+
+  AC_MSG_NOTICE([Looking for pubspec.yaml in ${SRC_DIR}])
+  if test -f "${SRC_DIR}/pubspec.yaml"; then
+    AC_MSG_NOTICE([Building dart .pub-cache ...])
+    (cd "${SRC_DIR}" && "${DART_HOME}/bin/dart" pub get --offline) || \
+    (cd "${SRC_DIR}" && "${DART_HOME}/bin/dart" pub get) || \
+    SF_MISSING_DEP([Failed to get public dart dependencies])
+  fi
 ])
--- dart-sdk/.sipxecs.mk
+++ dart-sdk/.sipxecs.mk
@@ -1,34 +1,30 @@
-#
-# You must install FPM by running
-#    gem install fpm
-#
-lib_all += dart-sdk
 
-dart-sdk_VER = 1.15.0
-dart-sdk_REL = 316768
-dart-sdk_TARBALL = dartsdk-linux-$(DISTRO_ARCH)-$(dart-sdk_VER)-r$(dart-sdk_REL).tar.gz
-dart-sdk_i386_RPM = dart-sdk-$(dart-sdk_VER)-$(dart-sdk_REL).i386.rpm
+dart-sdk_VER = 3.7.0
+dart-sdk_REL = 1
+dart-sdk_TARBALL = dartsdk-linux-x64-release.zip
+dart-sdk_URL = https://storage.googleapis.com/dart-archive/channels/stable/release/$(dart-sdk_VER)/sdk/$(dart-sdk_TARBALL)
 dart-sdk_x86_64_RPM = dart-sdk-$(dart-sdk_VER)-$(dart-sdk_REL).x86_64.rpm
-dart-sdk_intermediate_RPM = dart-sdk-$(dart-sdk_VER)-$(dart-sdk_REL).noarch.rpm
 
-dart-sdk.rpm-setup : fpm-setup
+# Install FPM
+fpm-install:
+	@if ! command -v fpm >/dev/null 2>&1; then \
+	    echo "Installing FPM..."; \
+	    gem install --no-document fpm; \
+	else \
+	    echo "FPM is already installed."; \
+	fi
+
+dart-sdk.rpm-setup: fpm-install
 
 dart-sdk.dist dart-sdk.srpm :;
 
-# separate copy step is needed because LHS of rpm target cannot include $(MOCK_RESULTS_DIR)
-# because it's defined AFTER this make file is loaded.
-dart-sdk.rpm : $(dart-sdk_$(DISTRO_ARCH)_RPM);
+dart-sdk.rpm: $(dart-sdk_x86_64_RPM)
 	mv -f $< $(MOCK_RESULTS_DIR)
 
-$(dart-sdk_x86_64_RPM) : $(DOWNLOAD_LIB_CACHE)/$(dart-sdk_TARBALL)
-	fpm -f -s tar -t rpm -n dart-sdk -v $(dart-sdk_VER) --iteration $(dart-sdk_REL) \
+$(dart-sdk_TARBALL): fpm-install
+	wget -O $(dart-sdk_TARBALL) $(dart-sdk_URL)
+
+$(dart-sdk_x86_64_RPM): $(dart-sdk_TARBALL)
+	fpm -f -s zip -t rpm -n dart-sdk -v $(dart-sdk_VER) --iteration $(dart-sdk_REL) \
 	  --after-install $(SRC)/dart-sdk/after-install.sh \
 	  --license BSD --prefix /opt -a $(DISTRO_ARCH) $<
-
-# I cannot generate 32 bit rpm on 64 bit machine because fpm tool will not let me.
-# Yes this assumes build host machine is 64bit
-$(dart-sdk_i386_RPM) : $(DOWNLOAD_LIB_CACHE)/$(dart-sdk_TARBALL)
-	fpm -f -s tar -t rpm -n dart-sdk -v $(dart-sdk_VER) --iteration $(dart-sdk_REL) \
-	  --after-install $(SRC)/dart-sdk/after-install.sh \
-	  --license BSD --prefix /opt -a noarch $<
-	mv $(dart-sdk_intermediate_RPM) $@
