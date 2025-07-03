726c0d90e342e7c7d3045fd89286a0b633ca6fea
iplahte <ivar.plahte@onrelay.com>
2025-07-03 18:14:42 +0200

    Patch for missing update response in sipXbridge

--- sipXbridge/src/main/java/org/sipfoundry/sipxbridge/CallControlManager.java
+++ sipXbridge/src/main/java/org/sipfoundry/sipxbridge/CallControlManager.java
@@ -952,6 +952,44 @@ class CallControlManager implements SymmitronResetHandler {
         }
     }
 
+        /**
+     * Process an incoming UPDATE.
+     *
+     * @param requestEvent -- the UPDATE request event.
+     */
+    private void processUpdate(RequestEvent requestEvent) {
+        try {
+            SipProvider provider = (SipProvider) requestEvent.getSource();
+            if ( logger.isDebugEnabled() ) logger.debug("processUpdate");
+
+            ServerTransaction serverTransactionId = requestEvent.getServerTransaction();
+            /*
+             * send 200 OK for UPDATE
+             */
+            Response updateOk = SipUtilities.createResponse(serverTransactionId, Response.OK);
+
+            Dialog dialog = requestEvent.getDialog();
+            DialogContext dialogContext = DialogContext.get(dialog);
+            ContactHeader requestContactHeader = (ContactHeader) requestEvent.getRequest().getHeader(ContactHeader.NAME);
+            String contactUser;   
+            if (requestContactHeader != null) {
+                SipURI contactURI = (SipURI) requestContactHeader.getAddress().getURI();
+                contactUser = contactURI.getUser();
+            }  else {
+                contactUser = Gateway.SIPXBRIDGE_USER;
+            }
+            
+            ItspAccountInfo itspAccount = dialogContext.getItspInfo();
+              ContactHeader contactHeader = SipUtilities.createContactHeader(provider, itspAccount,contactUser, serverTransactionId);
+            updateOk.setHeader(contactHeader);
+
+            serverTransactionId.sendResponse(updateOk);
+
+        } catch (Exception ex) {
+            logger.error("problem sending OK to UPDATE", ex);
+        }
+    }
+
     /**
      * Processes an INCOMING ack.
      *
@@ -3270,6 +3308,8 @@ logger.debug("SEv Delaying ACK");
             processRefer(requestEvent);
         } else if (method.equals(Request.PRACK)) {
             processPrack(requestEvent);
+        } else if (method.equals(Request.UPDATE)) {
+            processUpdate(requestEvent);
         }
 
     }
