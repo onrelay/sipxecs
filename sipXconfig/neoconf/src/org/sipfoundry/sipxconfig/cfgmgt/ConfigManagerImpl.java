6cad1c4f5e6d9ef422bf9aaffdb4a87066db3766
OnRelay <support@onrelay.net>
2025-11-18 08:12:34 +0000

    Reverted changes

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/cfgmgt/ConfigManagerImpl.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/cfgmgt/ConfigManagerImpl.java
@@ -89,7 +89,7 @@ public class ConfigManagerImpl implements AddressProvider, ConfigManager, BeanFa
     private final Object m_lock = new Object();
     // No strict host key checking is only for initial handshake. Once that passes, ssh will
     // check host name with key.
-    private String m_remoteCommand = "/usr/bin/ssh -o 'StrictHostKeyChecking=no' -i %s/.ssh/ppkeys/localhost.nopass.priv %s@%s";
+    private String m_remoteCommand = "/usr/bin/ssh -o 'StrictHostKeyChecking=no' -i %s/.cfagent/ppkeys/localhost.nopass.priv %s@%s";
     private String m_remoteHostsFile = "%s/.ssh/known_hosts";
     private boolean m_flag;
     private SystemAuditManager m_systemAuditManager;
