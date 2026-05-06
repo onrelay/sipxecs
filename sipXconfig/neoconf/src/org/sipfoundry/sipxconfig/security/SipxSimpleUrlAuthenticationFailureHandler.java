ab969f2050b0a3b56519a2575ed9b3df8fe2c26e
OnRelay Support <support@onrelay.net>
2026-05-06 12:47:35 +0000

    Fix null pointer exception with failed authentications

--- sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/security/SipxSimpleUrlAuthenticationFailureHandler.java
+++ sipXconfig/neoconf/src/org/sipfoundry/sipxconfig/security/SipxSimpleUrlAuthenticationFailureHandler.java
@@ -39,26 +39,27 @@ public class SipxSimpleUrlAuthenticationFailureHandler extends SimpleUrlAuthenti
         if( sipxDefaultFailureUrl != null ) {
             try {
                 String referer = request.getHeader(REFERER);
-                URL refererUrl = new URL(referer);
-                StringBuilder url = new StringBuilder();
-                String protocol = refererUrl.getProtocol();
-                url.append(protocol);
-                url.append("://");
-                url.append(refererUrl.getHost());
-                int port = refererUrl.getPort();
-                if (port > 0
-                        && ((protocol.equalsIgnoreCase("http") && port != 80)
-                        || (protocol.equalsIgnoreCase("https") && port != 443))) {
-                    url.append(':');
-                    url.append(port);
+                if( referer != null ) {
+                    URL refererUrl = new URL(referer);
+                    StringBuilder url = new StringBuilder();
+                    String protocol = refererUrl.getProtocol();
+                    url.append(protocol);
+                    url.append("://");
+                    url.append(refererUrl.getHost());
+                    int port = refererUrl.getPort();
+                    if (port > 0
+                            && ((protocol.equalsIgnoreCase("http") && port != 80)
+                            || (protocol.equalsIgnoreCase("https") && port != 443))) {
+                        url.append(':');
+                        url.append(port);
+                    }
+                    url.append(request.getContextPath());
+                    url.append(sipxDefaultFailureUrl);
+
+                    super.setDefaultFailureUrl( url.toString() );
                 }
-                url.append(request.getContextPath());
-                url.append(sipxDefaultFailureUrl);
-
-                super.setDefaultFailureUrl( url.toString() );
-
             } catch (Exception ex) {
-                this.logger.error("Error updating failure URL: " + ex.getMessage() );
+                this.logger.error("Error updating failure URL: " + ex.getMessage());
             }
         }
 
